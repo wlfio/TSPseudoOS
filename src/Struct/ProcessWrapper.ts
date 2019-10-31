1f1b97c601746cb0955515b1f38783d5a5264e78
WolfGang <wolfgang.voncaron@segura.co.uk>
2019-10-31 08:17:22 +0000

    pause

--- src/Struct/ProcessWrapper.ts
+++ src/Struct/ProcessWrapper.ts
@@ -2,13 +2,64 @@ import { IDisplay } from "../Services/Display";
 import { IProcessManager } from "../Services/ProcessManager";
 import FS from "../Services/FileSystem";
 import Process from "./Process";
-import { FunctionSignature, IDisplayItem } from "../App/libOS";
-import { OSApi } from "./Types";
+import { FunctionSignature, IDisplayItem, ILibOS, ILibFS, ILibStd } from "../App/libOS";
+import { OSApi, FunctionSet } from "./Types";
 
-class OSLib {
+class OSLib implements ILibOS {
+
+    private _call: (signature: FunctionSignature, data: any) => Promise<any>;
+
+    Std: ILibStd = {
+        out: (data: any) => this.call("Std", "out", data),
+        in: (pid: number, data: any, source?: string | number) => this.call("Std", "in", { pid, data, source }),
+    };
+    FS: ILibFS = {
+        list: (path: string) => this.call("FS", "list", path),
+        mkdir: (path: string) => this.call("FS", "mkdir", path),
+        resolve: (paths: string[]) => this.call("FS", "resolve", paths),
+        append: (path: string, content: string) => this.call("FS", "append", { path, content }),
+        write: (path: string, content: string) => this.call("FS", "write", { path, content }),
+        touch: (path: string) => this.call("FS", "touch", path),
+        read: (path: string) => this.call("FS", "read", path),
+        del: (path: string) => this.call("FS", "del", path),
+        dirExists: (path: string) => this.call("FS", "dirExists", path),
+        fileExists: (path: string) => this.call("FS", "fileExists", path),
+        delDir: (path: string) => this.call("FS", "delDir", path),
+    };
+    Process: FunctionSet = {
+        end: (process: Process) => process.kill(),
+        setSelf: (process: Process, data: { prop: string, value: any }) => process.set(data.prop, data.value),
+        self: (process: Process) => Promise.resolve(process.data()),
+        start: (process: Process, data: any) => this.manager.startProcess(data.exec, data.params, null, process),
+        crash: (process: Process, error: any) => { this.api.Std.out(process, error); return process.kill(); },
+        changeWorkingDir: (process: Process, data: any) => process.changeWorkingDir(data),
+    };
+    Display: FunctionSet = {
+        // @ts-ignore
+        prompt: (process: Process, text: string) => this.display.setText(text),
+        // @ts-ignore
+        print: (process: Process, item: IDisplayItem) => this.display.output(item.data, item.over, false),
+        // @ts-ignore
+        printLn: (process: Process, item: IDisplayItem) => this.display.output(item.data, item.over, true),
+        info: () => this.display !== null ? this.display.info() : Promise.reject(new Error("PM Error : missing display : ...")),
+    };
+
+    constructor(process: Process, call: (signature: FunctionSignature, data: any) => Promise<any>) {
+        this._call = call;
+    }
+
+    call(service: string, func: string, data: any): Promise<any> {
+        return this._call({ service: service, func: func }, data);
+    }
+
+}
+
+export default class ProcessWrapper {
     display: IDisplay;
     manager: IProcessManager;
-    wrapper: ProcessWrapper;
+    lib: OSLib;
+
+    process: Process;
 
     private api: OSApi = {
         Std: {
@@ -48,35 +99,27 @@ class OSLib {
         }
     };
 
-    constructor(wrapper: ProcessWrapper, display: IDisplay, manager: IProcessManager) {
-        this.wrapper = wrapper;
-        this.display = display;
+    constructor(manager: IProcessManager, display: IDisplay, process: Process) {
         this.manager = manager;
+        this.display = display;
+        this.process = process;
+        this.lib = new OSLib(process, (signature: FunctionSignature, data: any) => this.call(signature, data));
     }
+
 
     call(signature: FunctionSignature, data: any): Promise<any> {
         if (this.api.hasOwnProperty(signature.service)) {
             if (this.api[signature.service].hasOwnProperty(signature.func)) {
-                return this.api[signature.service][signature.func](this.wrapper.process, data);
+                return this.api[signature.service][signature.func](this.process, data);
             }
         }
         return Promise.reject(
             new Error([
                 "PM Error",
                 "Atempt to access non-existant System Call\n" + signature.service + ">" + signature.func,
-                this.wrapper.process.exec + "[" + this.wrapper.process.id + "]"
+                this.process.exec + "[" + this.process.id + "]"
             ].join(" : "))
         );
-    }
-}
-
-class ProcessWrapper {
-    api: OSLib;
-    process: Process;
-
-    constructor(manager: IProcessManager, display: IDisplay, process: Process) {
-        this.process = process;
-        this.api = new OSLib(this, display, manager);
     }
 }
 
--- src/Struct/Types.ts
+++ src/Struct/Types.ts
@@ -1,4 +1,5 @@
 import Process from "./Process";
 
-export type FunctionSet = { [s: string]: { [s: string]: Function } };
-export type OSApi = { [s: string]: { [s: string]: (process:Process, data:any) => Promise<any> } };
+export type FunctionSetSet = { [s: string]: FunctionSet };
+export type FunctionSet = { [s: string]: Function };
+export type OSApi = { [s: string]: { [s: string]: (process: Process, data: any) => Promise<any> } };
